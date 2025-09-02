3ac63ceaaa4dfa4acd1e72d84ca795faf1e88da9
morteza <morteza.rostami.cs@gmail.com>
2025-09-02 14:19:11 -0700

    add production-ready Dockerfiles for backend and frontend

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+};
+
+export default config;
